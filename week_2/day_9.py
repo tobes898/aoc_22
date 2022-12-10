4b563546e3c7aa0466adf61ca3ad7fc537ad18b2
tobes898 <adventureguy898@gmail.com>
2022-12-10 17:06:31 -0500

    Day 9 but with issues

--- week_2/day_9.py
+++ week_2/day_9.py
@@ -12,7 +12,7 @@ def move_H(grid, x_pos, y_pos, current_pos_H):
     current_pos_H[0] = x_pos
     current_pos_H[1] = y_pos
 
-def calc_H_movement(grid, movement, current_pos_H, current_pos_T):
+def calc_H_movement(grid, movement, current_pos_H, current_pos_T, list_of_tails):
     direction = movement[0].upper()
     num = int(movement[1])
     x_pos = current_pos_H[0]
@@ -27,7 +27,13 @@ def calc_H_movement(grid, movement, current_pos_H, current_pos_T):
         while x_pos < new_x_pos:
             x_pos += 1
             move_H(grid, x_pos, y_pos, current_pos_H)
-            count = calc_T_movement(grid,current_pos_T,current_pos_H, count)
+            calc_T_movement(grid,list_of_tails[0],current_pos_H, False)
+            for i in range(1, len(list_of_tails)):
+                if i == len(list_of_tails) - 1:
+                    calc_T_movement(grid, list_of_tails[i],list_of_tails[i-1], True)
+                else:
+                    calc_T_movement(grid, list_of_tails[i],list_of_tails[i-1], False)
+
 
     # L -> x - n
     if direction == 'L':
@@ -36,7 +42,12 @@ def calc_H_movement(grid, movement, current_pos_H, current_pos_T):
         while x_pos > new_x_pos:
             x_pos -= 1
             move_H(grid, x_pos, y_pos, current_pos_H)
-            count = calc_T_movement(grid,current_pos_T,current_pos_H, count)
+            calc_T_movement(grid,list_of_tails[0],current_pos_H, False)
+            for i in range(1, len(list_of_tails)):
+                if i == len(list_of_tails) - 1:
+                    calc_T_movement(grid, list_of_tails[i],list_of_tails[i-1], True)
+                else:
+                    calc_T_movement(grid, list_of_tails[i],list_of_tails[i-1], False)
 
 
 
@@ -47,7 +58,14 @@ def calc_H_movement(grid, movement, current_pos_H, current_pos_T):
         while y_pos > new_y_pos:
             y_pos -= 1
             move_H(grid, x_pos, y_pos, current_pos_H)
-            count =calc_T_movement(grid,current_pos_T,current_pos_H, count)
+            # move list 
+            calc_T_movement(grid,list_of_tails[0],current_pos_H, False)
+            for i in range(1, len(list_of_tails)):
+                if i == len(list_of_tails) - 1:
+                    calc_T_movement(grid, list_of_tails[i],list_of_tails[i-1], True)
+                else:
+                    calc_T_movement(grid, list_of_tails[i],list_of_tails[i-1], False)
+
 
 
 
@@ -58,11 +76,17 @@ def calc_H_movement(grid, movement, current_pos_H, current_pos_T):
         while y_pos < new_y_pos:
             y_pos += 1
             move_H(grid, x_pos, y_pos, current_pos_H)
-            count =calc_T_movement(grid,current_pos_T,current_pos_H, count)
+            calc_T_movement(grid,list_of_tails[0],current_pos_H, False)
+            for i in range(1, len(list_of_tails)):
+                if i == len(list_of_tails) - 1:
+                    calc_T_movement(grid, list_of_tails[i],list_of_tails[i-1], True)
+                else:
+                    calc_T_movement(grid, list_of_tails[i],list_of_tails[i-1], False)
 
-    print(count)
 
-def move_T_left_right(grid, current_pos_T, x_diff):
+    # print(count)
+
+def move_T_left_right(grid, current_pos_T, x_diff, track):
     new_x_pos = current_pos_T[0]
     if x_diff < -1:
         new_x_pos += 1
@@ -72,12 +96,13 @@ def move_T_left_right(grid, current_pos_T, x_diff):
     # update grid with new T location
     grid[current_pos_T[1]][new_x_pos] = 'T'
     # update visited
-    # movement_tracker[current_pos_T[1]][new_x_pos] = '#'
+    if track:
+        movement_tracker[current_pos_T[1]][new_x_pos] = '#'
     # update grid to set prev location back to .
     grid[current_pos_T[1]][current_pos_T[0]] = '.'
     current_pos_T[0] = new_x_pos
 
-def move_T_up_down(grid, current_pos_T, y_diff):
+def move_T_up_down(grid, current_pos_T, y_diff, track):
     new_y_pos = current_pos_T[1]
     if y_diff < -1:
         new_y_pos += 1
@@ -86,7 +111,8 @@ def move_T_up_down(grid, current_pos_T, y_diff):
     
     grid[new_y_pos][current_pos_T[0]] = 'T'
     
-    # movement_tracker[new_y_pos][current_pos_T[0]] = '#'
+    if track:
+        movement_tracker[new_y_pos][current_pos_T[0]] = '#'
 
     grid[current_pos_T[1]][current_pos_T[0]] = '.'
     current_pos_T[1] = new_y_pos
@@ -104,7 +130,8 @@ def move_T_up_down(grid, current_pos_T, y_diff):
 #bottom left -1 x & +1y
 #diff +1x & -2y
 
-def move_T_diagonal(grid, current_pos_T, x_difference, y_difference):
+def move_T_diagonal(grid, current_pos_T, x_difference, y_difference, track):
+    # need to add more to x difference here 
     new_x_pos = current_pos_T[0]
     new_y_pos = current_pos_T[1]
     if x_difference == -1:
@@ -113,23 +140,37 @@ def move_T_diagonal(grid, current_pos_T, x_difference, y_difference):
             new_y_pos += 1
         else:
             new_y_pos -= 1
-    else:
+    elif x_difference == 1:
         new_x_pos -= 1
         if y_difference < -1:
             new_y_pos += 1
         else:
             new_y_pos -= 1
+    
+    elif y_difference == -1:
+        new_y_pos+=1
+        if x_difference < -1:
+            new_x_pos += 1
+        else:
+            new_x_pos -= 1
+    elif y_difference == 1:
+        new_y_pos -= 1
+        if x_difference < -1:
+            new_x_pos += 1
+        else:
+            new_x_pos -= 1
 
     grid[new_y_pos][new_x_pos] = 'T'
-    # movement_tracker[new_y_pos][new_x_pos] = '#'
+    if track:
+        movement_tracker[new_y_pos][new_x_pos] = '#'
     grid[current_pos_T[1]][current_pos_T[0]] = '.'
     current_pos_T[0] = new_x_pos
     current_pos_T[1] = new_y_pos
 
 
 
-def calc_T_movement(grid, current_pos_T, current_pos_H, count):
-    count += 1
+def calc_T_movement(grid, current_pos_T, current_pos_H, track):
+    # count += 1
 
     # note for tommorrow - issue is when index is 0
     # likely need increment everything by one when calculating difference
@@ -148,22 +189,22 @@ def calc_T_movement(grid, current_pos_T, current_pos_H, count):
     # adjust both (ie. diagonal movement)
     # adjust just x
     if abs(x_difference) > 1 and y_difference == 0:
-        move_T_left_right(grid, current_pos_T, x_difference)
+        move_T_left_right(grid, current_pos_T, x_difference, track)
     # adjust just y
     elif abs(y_difference) > 1 and x_difference == 0:
-        move_T_up_down(grid, current_pos_T, y_difference)
+        move_T_up_down(grid, current_pos_T, y_difference, track)
 
-    elif abs(x_difference) == 1 and abs(y_difference) > 1:
-        move_T_diagonal(grid, current_pos_T, x_difference, y_difference)
+    elif (abs(x_difference) == 1 or abs(y_difference) == 1) and (abs(y_difference) > 1 or abs(x_difference) > 1):
+        move_T_diagonal(grid, current_pos_T, x_difference, y_difference, track)
 
-    print('---------')
-    print_grid(grid)
-    print('---------')
-    print(current_pos_T)
-    print(current_pos_H)
-    return count
+    # print('---------')
+    # print_grid(grid)
+    # # print('---------')
+    # print(current_pos_T)
+    # print(current_pos_H)
+    # return count
 
-inputFile = open('./week_2/inputs/day9_even_smaller_input.txt', 'r')
+inputFile = open('./week_2/inputs/day9_input.txt', 'r')
 
 movement = []
 for line in inputFile.readlines():
@@ -174,8 +215,8 @@ assert len(movement) > 0
 #step by step move head and then check what movement tail needs to stay correct
 
 # create grid
-x_len = 6
-y_len = 5
+x_len = 1000
+y_len = 1000
 
 grid = []
 global movement_tracker
@@ -188,24 +229,54 @@ for i in range(0, y_len):
     for j in range(0, x_len):
         row.append('.')
     grid.append(row)
+
+
+for i in range(0, y_len):
+    row = []
+    for j in range(0, x_len):
+        row.append('.')
     movement_tracker.append(row)
 
-# start is bottom left of grid so (0, len(y) -1)
-current_pos_H = [0, y_len-1]
-current_pos_T = [0, y_len-1]
-# movement_tracker[0][y_len-1] = 's'
-grid[y_len-1][0] = 'H'
 
+starting_x = int(x_len / 500) -1
+starting_y = int(y_len / 500) - 1
+starting_x = 15
+starting_y = 11
+# start is bottom left of grid so (0, len(y) -1)
+rope_parts = []
+amount_of_parts = 10
+
+for i in range(0, amount_of_parts):
+    part = [starting_x, starting_y]
+    rope_parts.append(part)
+
+# need way to only do visit grid on last tail? or grid for each tail
+current_pos_H = [starting_x, starting_y]
+# current_pos_T = [starting_x, starting_y]
+# movement_tracker[0][y_len-1] = 's'
+grid[starting_y][starting_x] = 'H'
+
+movement_tracker[starting_y][starting_x] = 's'
 for move in movement:
  
-    calc_H_movement(grid, move, current_pos_H, current_pos_T)
+    calc_H_movement(grid, move, current_pos_H, rope_parts[0], rope_parts)
     # calc_T_movement(grid, current_pos_T, current_pos_H)
 
 
 
-print_grid(grid)
+# print_grid(grid)
+# print_grid(movement_tracker)
 # print_grid(movement_tracker)
 # print(count)
 #if difference between T and H is 2 then T needs to move closer to H
 #need to execute each move individually
+#420
+#451 too low
+#510
+count = 0
+for i in range(0, len(movement_tracker)):
+    for j in range(0, len(movement_tracker[0])):
+        if movement_tracker[i][j] == '#' or movement_tracker[i][j] == 's':
+            count += 1
 
+print(count)
